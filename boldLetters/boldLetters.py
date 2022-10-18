92c9c7af7e19bbbae73f5ac0b94ac6d40aa80743
Res92 <res92722@gmail.com>
2022-10-18 19:50:41 +0700

    first done

---
+++ boldLetters/boldLetters.py
@@ -0,0 +1,28 @@
+import pyperclip
+import random as r
+
+with open('a.txt', 'r', encoding='utf-8') as f:
+    l = f.read()
+    s = ""
+    j = 0
+    for i in range(len(l)):
+        if l[i] == " ":
+            s += " "
+            j += 1
+        elif l[i] in '(),:"-[]|>': 
+            s += l[i]
+            j += r.randrange(2)
+        elif l[i] == "\n":
+            s += "\n"
+            j += r.randrange(2)
+        elif i == j and not (l[i] in '(),:"-[]|>') and not (l[i] == "\n"):
+            s += f'**{l[i]}**'
+            j -= r.randrange(3, 6)
+        else:
+            #print(l[i])
+            s += l[i]
+            j += 2
+
+    # with open('b.txt', 'w', encoding='utf-8') as f: f.write(s)
+    print(s)
+    pyperclip.copy(s)
---
+++ boldLetters/boldLetters2.py
@@ -0,0 +1,23 @@
+from secrets import randbelow
+import pyperclip
+import random as r
+
+with open('a.txt', 'r', encoding='utf-8') as f:
+    l = f.read()
+    splt = l.split(' ')
+    radBold = 0
+    s = ''
+
+    for i in range(len(splt)):
+        lenLett = len(splt[i]); #print(splt[i])
+        radBold = r.randrange(lenLett)
+
+        for j in range(len(splt[i])):
+            if splt[i][j] in '\n"(),:-[]|>./':
+                radBold = r.randrange(lenLett)
+            elif j == radBold:
+                s += f'**{splt[i][j]}**'; continue
+            s += splt[i][j]
+        s += ' '
+    #with open('b.txt', 'w', encoding='utf-8') as f: f.write(s)
+    print(s); pyperclip.copy(s)
